2e7f335e02632c73b463e449b4644d895d844881
mskovn <mett088d@stud.kea.dk>
2018-12-18 18:22:34 +0100

    -

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function carousel() {
         myIndex = 1
     }
     x[myIndex - 1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(carousel, 5000); // Change image every 2 seconds
 }
 
 
